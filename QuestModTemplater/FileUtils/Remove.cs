03a0edf34a09031ed73529412c1e4a94f80cb80d
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 08:28:51 +0100

    Added function to remove read-only dirs

---
+++ QuestModTemplater/FileUtils/Remove.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace QuestModTemplater.FileUtils
+{
+    class Remove
+    {
+        public static void RemoveReadOnlyDir(string dir)
+        {
+            foreach (var subdir in Directory.EnumerateDirectories(dir))
+            {
+                RemoveReadOnlyDir(subdir);
+            }
+            foreach (var file in Directory.EnumerateFiles(dir))
+            {
+                var fileInfo = new FileInfo(file);
+                fileInfo.Attributes = FileAttributes.Normal;
+                fileInfo.Delete();
+            }
+            Directory.Delete(dir);
+        }
+    }
+}
