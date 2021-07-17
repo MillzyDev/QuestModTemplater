5e4b852f99a872d70f6d1184d57134a04273c7ba
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 11:56:11 +0100

    Create Program.cs

---
+++ QuestModTemplater/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using QuestModTemplater;
+
+namespace QuestModTemplater
+{
+    class Program
+    {
+        public static List<ICommand> commands = new List<ICommand>
+        {
+            new Commands.Clear(),
+            new Commands.Create(),
+            new Commands.Help(),
+            new Commands.Update()
+        };
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                // Call help command
+                Console.WriteLine("Command not found...");
+                commands[2].handle();
+            }
+            // Check if command is valid command
+            foreach (ICommand command in commands)
+            {
+                if (command.getName() == args[0])
+                {
+                    // Execute command
+                    command.handle();
+                    return;
+                }
+                // Call help command
+                Console.WriteLine("Command not found...");
+                commands[2].handle();
+            }
+        }
+    }
+}
