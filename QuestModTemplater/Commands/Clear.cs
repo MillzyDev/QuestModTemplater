6f9ce5d9a4c072e2b08d6bc525f2179ddd7b5688
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 08:55:20 +0100

    Create Clear.cs

---
+++ QuestModTemplater/Commands/Clear.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace QuestModTemplater.Commands
+{
+    class Clear : ICommand
+    {
+        public string getDescription()
+        {
+            return "Clears the cloned mod-template-repository";
+        }
+
+        public string getName()
+        {
+            return "clear";
+        }
+
+        public void handle()
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var path = Path.Combine(appDataPath, @"QuestModTemplater\");
+            Console.WriteLine("Checking if AppData directory exists...");
+            if (!Directory.Exists(path)) 
+            { 
+                Console.WriteLine("AppData directory does not exist.");
+                return;
+            }
+
+            var templatePath = Path.Combine(path, @"quest-mod-template\");
+            Console.WriteLine("Checking if quest-mod-template directory exists...");
+            if (!Directory.Exists(templatePath))
+            {
+                Console.WriteLine("quest-mod-template directory does not exist.");
+                return;
+            }
+
+            Console.WriteLine("Purging quest-mod-template directory...");
+
+            FileUtils.Remove.RemoveReadOnlyDir(Path.Combine(templatePath, @".git\"));
+            Directory.Delete(templatePath, true);
+
+            Console.WriteLine("Creating quest-mod-template directory...");
+            Directory.CreateDirectory(templatePath);
+
+            Console.WriteLine("Cleared quest-mod-template!");
+        }
+    }
+}
