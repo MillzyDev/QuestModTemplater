1b9dc27e7fe3b36738b07dc69b6b0e120d25ba20
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 08:21:45 +0100

    Create Update.cs

---
+++ QuestModTemplater/Commands/Update.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using LibGit2Sharp;
+
+using QuestModTemplater;
+
+namespace QuestModTemplater.Commands
+{
+    class Update : ICommand
+    {
+        string ICommand.getDescription()
+        {
+            return "Updates the downloaded template, or redownloads it.";
+        }
+
+        string ICommand.getName()
+        {
+            return "update";
+        }
+
+        void ICommand.handle()
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var path = Path.Combine(appDataPath, @"QuestModTemplater\");
+            Console.WriteLine("Checking if AppData directory exists...");
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+            var templatePath = Path.Combine(path, @"quest-mod-template\");
+            Console.WriteLine("Checking if quest-mod-template directory exists...");
+            if (!Directory.Exists(templatePath))
+            {
+                Console.WriteLine("Creating quest-mod-template directory...");
+                Directory.CreateDirectory(templatePath);
+            } else
+            {
+                Console.WriteLine("Purging quest-mod-template directory...");
+
+                FileUtils.Remove.RemoveReadOnlyDir(Path.Combine(templatePath, @".git\"));
+                Directory.Delete(templatePath, true);
+
+                Console.WriteLine("Creating quest-mod-template directory...");
+                Directory.CreateDirectory(templatePath);
+            }
+
+            Console.WriteLine($"Cloning repository from {BaseValues.baseUrl}...");
+                Repository.Clone(BaseValues.baseUrl, templatePath);
+
+            Console.WriteLine("Cloned quest-mod-template!");
+        }
+    }
+}
