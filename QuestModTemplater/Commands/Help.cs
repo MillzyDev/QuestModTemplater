aebee00ee2ffce9bad643942027fe66966d44a8b
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 09:04:32 +0100

    Create Help.cs

---
+++ QuestModTemplater/Commands/Help.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestModTemplater.Commands
+{
+    class Help : ICommand
+    {
+        public string getDescription()
+        {
+            return "Displays a list of commands and their purposes.";
+        }
+
+        public string getName()
+        {
+            return "help";
+        }
+
+        public void handle()
+        {
+            foreach (ICommand command in Program.commands)
+            {
+                Console.WriteLine($"{command.getName()} - {command.getDescription()}");
+            }
+        }
+    }
+}
