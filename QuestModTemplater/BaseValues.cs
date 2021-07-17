689ae56b71d85a4128915d1336441a903fe1b6c1
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 08:21:30 +0100

    Create BaseValues.cs

---
+++ QuestModTemplater/BaseValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestModTemplater
+{
+    class BaseValues
+    {
+        public static String baseUrl = "https://github.com/Lauriethefish/quest-mod-template.git";
+    }
+}
