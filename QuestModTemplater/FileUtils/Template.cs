77c5ae89b9d52c2d83e8a0efc752c24bf7501e34
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 10:36:42 +0100

    Create Template.cs

---
+++ QuestModTemplater/FileUtils/Template.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace QuestModTemplater.FileUtils
+{
+    class Template
+    {
+        public static void CopyFilesRecursively(string src, string trg)
+        {
+            // Create all the directories
+            foreach (string dirPath in Directory.GetDirectories(src, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(src, trg));
+            }
+            // Copy and overwrite files
+            foreach (string newPath in Directory.GetFiles(src, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(src, trg), true);
+            }
+        }
+    }
+}
