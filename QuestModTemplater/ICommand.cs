709882bfc7bb6fdc666e462d8352fd31722838d1
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-17 08:21:37 +0100

    Create ICommand.cs

---
+++ QuestModTemplater/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestModTemplater
+{
+    interface ICommand
+    {
+        public String getName();
+        public String getDescription();
+
+        public void handle();
+
+    }
+}
